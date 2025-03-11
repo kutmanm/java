8f5f35c20c3202780115815bc7a2418b88c85803
kutmanm <iandrugsdeath@gmail.com>
2025-03-11 16:41:03 +0600

    StudentManagementSystem.java

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -112,7 +112,7 @@ class StudentManager {
     }
 }
 
-public class Main {
+public class StudentManagementSystem {
     public static void main(String[] args) {
         StudentManager manager = new StudentManager();
 
